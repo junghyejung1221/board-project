9cd2bb6a4b5d55d82942406c37967bd2254ad3bf
decide_jung <jung991221@naver.com>
2022-10-18 17:28:37 +0900

    #10 - 도메인 코드를 엔티티로 변환
    도메인 클래스 == 1테이블 매칭되도록 설계함

--- src/main/java/com/campus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/campus/projectboard/domain/ArticleComment.java
@@ -1,15 +1,83 @@
 package com.campus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "createBy"),
+} )
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class ArticleComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter @ManyToOne(optional = false)
     private Article article;  //게시글
+    @Setter @Column(nullable = false, length = 500)
     private String content;  //내용
 
-    private LocalDateTime createAt;  // 생성일시
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+
+    @CreatedDate
+    @Column(nullable = false )
+    private LocalDateTime createAt;  //생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createBy;  //생성자
+    @LastModifiedDate
+    @Column(nullable = false )
     private LocalDateTime modifiedAt;  //수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;  //수정자
 
+    protected ArticleComment() {
+    }
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment)) return false;
+        ArticleComment that = (ArticleComment) o;
+        return id !=null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
